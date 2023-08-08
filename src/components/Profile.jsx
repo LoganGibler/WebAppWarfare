fb60d59d04e9340449d7a60238062637c8bbab61
LoganGibler <Logan.Gibler@gmail.com>
2023-08-08 18:59:40 -0400

    some buttons changed, going to do a test deployment

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -20,44 +20,46 @@ const Profile = ({ userBlogs }) => {
       <div className="main-profile-container">
         {userBlogs.length ? (
           <div>
-            <h4 className="profile-title"> Welcome back, {activeUser}!</h4>
-            <p>
+            <h5 className="profile-title"> Welcome back, {activeUser}!</h5>
+            <p className="profile-paragraph">
               Looks like you already have some created guides! You can add and
               edit steps of your guides here by clicking on the guide you want
-              to edit. Remember to publish your guide so others can see it!s
+              to edit. Remember to publish your guide so others can see it!
             </p>
           </div>
         ) : null}
-        {userBlogs.length ? (
-          userBlogs.map((blog) => {
-            // console.log(blog);
-            return (
-              <div className="profile-guide-main-div" key={blog._id}>
-                <div
-                  className="blog-profile-div"
-                  key={blog._id}
-                  onClick={() => {
-                    history.push(`/userguides/${blog._id}`);
-                  }}
-                >
-                  <div className="title-date-div">
-                    <h4 className="vmtitle-profile">{blog.vmtitle}</h4>
-                    <p className="date-profile">Created: {blog.date}</p>
+        <div className="profile-listed-guides">
+          {userBlogs.length ? (
+            userBlogs.map((blog) => {
+              // console.log(blog);
+              return (
+                <div className="profile-guide-main-div" key={blog._id}>
+                  <div
+                    className="blog-profile-div"
+                    key={blog._id}
+                    onClick={() => {
+                      history.push(`/userguides/${blog._id}`);
+                    }}
+                  >
+                    <div className="title-date-div">
+                      <h6 className="vmtitle-profile">{blog.vmtitle}</h6>
+                      <p className="date-profile">Created: {blog.date}</p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })
-        ) : (
-          <div>
-            <h4> Welcome to WebAppWarfare, {activeUser}!</h4>
-            <p>
-              Looks like you don't have any created guides! Click here to create
-              one:
-            </p>
-            <a href="/Postform"> Click here to create a Guide</a>
-          </div>
-        )}
+              );
+            })
+          ) : (
+            <div className="profile-no-guides-created-div">
+              <h5> Welcome to WebAppWarfare, {activeUser}!</h5>
+              <p className="profile-no-guides-p">
+                Looks like you don't have any created guides! Click here to
+                create one:
+              </p>
+              <a href="/Postform"> Click here to create a Guide</a>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
