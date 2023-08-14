3a8bfb8b81dec4741711b7931c9781b9c732fdbd
LoganGibler <Logan.Gibler@gmail.com>
2023-08-14 13:38:59 -0400

    -

--- server.js
+++ server.js
@@ -397,7 +397,7 @@ app.post("/sendFeedback", async (req, res) => {
 });
 
 mongoose
-  .connect("mongodb+srv://baseUsers:z1x2c3v@webappwarfare.px8ftut.mongodb.net/?retryWrites=true&w=majority")
+  .connect("mongodb+srv://baseUsers:z1x2c3v@webappwarfare.px8ftut.mongodb.net/")
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
       console.log(`server is running on port: ${process.env.PORT}`);
